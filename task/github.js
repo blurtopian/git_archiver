1f0b7f130925e5784d7f75d79e055437d14ee6ed
East <eastmaels@gmail.com>
2024-07-25 08:39:53 +0900

    add repo file size checker

--- task/github.js
+++ task/github.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const GITHUB_API_URL = 'https://api.github.com';
 const ACCESS_TOKEN = process.env.GIT_ACCESS_TOKEN; // Replace with your actual token
 const TEST_KEYWORD = process.env.TEST_KEYWORD; // You can change this to any keyword you like
+const MAX_REPO_SIZE_BYTES = 10 * 1024 * 1024; // Example condition: 100 MB
 
 async function searchRandomRepo() {
   try {
@@ -26,11 +27,29 @@ async function searchRandomRepo() {
     }
 
     const data = response.data;
+    const repositories = data.items;
 
-    if (data.items && data.items.length > 0) {
-      const randomIndex = Math.floor(Math.random() * data.items.length);
-      const randomRepo = data.items[randomIndex];
-      return randomRepo;
+    if (repositories && repositories.length > 0) {
+      let foundRepo = false;
+
+      while (!foundRepo) {
+        const randomIndex = Math.floor(Math.random() * repositories.length);
+        const randomRepo = repositories[randomIndex];
+         // Remove the selected repository from the list
+        repositories.splice(randomIndex, 1);
+
+        // Size is returned in KB, convert to bytes
+        const repoSizeBytes = randomRepo.size * 1024;
+        console.log(`Repository: ${randomRepo.full_name}, Size: ${repoSizeBytes} bytes`);
+
+        if (repoSizeBytes <= MAX_REPO_SIZE_BYTES) {
+          return randomRepo;
+        } else {
+          console.log(`Repository ${repo.full_name} is too large to archive.`);
+          continue;
+        }
+      }
+
     } else {
       console.log('No repositories found.');
     }
